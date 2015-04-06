a72d05d27b0140b49c67c35020ac48b96f6ef111
akeimach <akeimach@umich.edu>
2015-04-06 00:49:42 -0400

    determineSchema update
    implement detect input type

--- DatabaseGUI.java
+++ DatabaseGUI.java
@@ -3,10 +3,8 @@ import javax.swing.JFileChooser;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.ImageIcon;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JFrame;
-import javax.swing.JComponent;
 import javax.swing.JTextArea;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingUtilities;
@@ -15,28 +13,26 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
-import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-
  
 public class DatabaseGUI extends JPanel {
 	
+	private static final long serialVersionUID = 1L;
+	
+	public static JTextArea filePath = new JTextArea();
+	public static JTextArea fileContents = new JTextArea();
+	public static ArrayList<Object> defaultVals = new ArrayList<Object>(); 
+	
+	public static int maxCols = 0;
+	//public static String[] defaultVals = null;
+	
 	static File path;
-	static DetermineSchema determine;
-	static ArrayList<ArrayList<String>> rows = new ArrayList<ArrayList<String>>();
 	private JPanel tabLoad;
-	private JPanel tabQuery;
-	//public static JTextArea fileContents;
-	//public static JTextArea filePath;
-	//public static File data;
-	//final JTextArea fileContents = new JTextArea();
+	//private JPanel tabQuery;
 	
     public DatabaseGUI() {
     	
@@ -49,6 +45,8 @@ public class DatabaseGUI extends JPanel {
         tabbedPane.addTab("Load file", null, tabLoad, null);
         tabLoad.setPreferredSize(new Dimension(600, 400));
         tabLoadContents(tabLoad);
+        
+        
         
         JPanel tabQuery = new JPanel();
         tabQuery.setLayout(null);
@@ -64,9 +62,8 @@ public class DatabaseGUI extends JPanel {
     }
      
     public static JPanel tabLoadContents(JPanel tabLoad) {
+    		
     	
-    	
-    	final JTextArea filePath = new JTextArea();
     	filePath.setEditable(false);
     	filePath.setForeground(Color.LIGHT_GRAY);
     	filePath.setText(" Select data file to upload");
@@ -74,12 +71,9 @@ public class DatabaseGUI extends JPanel {
         tabLoad.add(filePath);
         
         JScrollPane scrollInput = new JScrollPane();
-        scrollInput.setBounds(24, 48, 535, 121);
+        scrollInput.setBounds(24, 48, 535, 56);
         scrollInput.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         tabLoad.add(scrollInput);
-        
-        final JTextArea fileContents = new JTextArea();
-        //final JTextArea fileContents = new JTextArea();
         scrollInput.setViewportView(fileContents);
         
         JButton btnBrowse = new JButton("Browse");
@@ -91,51 +85,35 @@ public class DatabaseGUI extends JPanel {
         		if (val == JFileChooser.APPROVE_OPTION) {
         			path = fileChooser.getSelectedFile();
         			filePath.setText(path.getAbsolutePath());
-					try {
-						BufferedReader readData;
-						readData = new BufferedReader(new FileReader(path));
-						String line = null;
-						for (int i = 0; i < 30; ++i) {
-							line = readData.readLine();
-							fileContents.append(line +"\n");							
-							//determine.DetermineSchema(line, rows);
-							System.out.println("ArrayList: " + determine.DetermineSchema(line, rows));
-							
-						}
-						readData.close();
-					} catch (FileNotFoundException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}				
+        			try { 
+        				DetermineSchema.getSchema(path.getAbsolutePath()); 
+        				DetermineSchema.guessTitles(defaultVals);
+        			} 
+        			catch (IOException e) { e.printStackTrace(); }			
         		}
+        		
         	}
         });
         btnBrowse.setBounds(431, 6, 128, 29);
         tabLoad.add(btnBrowse);
         
-        
-        JScrollPane scrollOutput = new JScrollPane();
-        scrollOutput.setBounds(24, 181, 535, 126);
-        scrollOutput.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-        tabLoad.add(scrollOutput);
-        
-        JTextArea textArea = new JTextArea();
-        scrollOutput.setViewportView(textArea);
-        
-        
-        
         JButton btnNext = new JButton("Accept schema");
-        btnNext.setBounds(431, 319, 128, 29);
+        btnNext.setBounds(431, 314, 128, 29);
         tabLoad.add(btnNext);
+        
+        JButton btnReparse = new JButton("Re-parse");
+        btnReparse.setBounds(34, 314, 128, 29);
+        tabLoad.add(btnReparse);
+              
+        
+        
+        
         
 		return tabLoad;
 		
     }
     
-    public static JPanel tabQueryContents(JPanel tabQuery) {
+	public static JPanel tabQueryContents(JPanel tabQuery) {
     	return tabQuery;
     }  
      
@@ -165,8 +143,6 @@ public class DatabaseGUI extends JPanel {
      
     public static void main(String[] args) {
     	
-    	
-        
         //create and show this application's GUI
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
--- DetermineSchema.java
+++ DetermineSchema.java
@@ -1,31 +1,86 @@
+import java.io.*;
 import java.util.ArrayList;
+import java.util.regex.Pattern;
+import org.apache.commons.csv.CSVParser;
 
-public class DetermineSchema extends DatabaseGUI {
+public class DetermineSchema {
 	
-	ArrayList<ArrayList<String>> rows = new ArrayList<ArrayList<String>>();
+	//TINYINT, SMALLINT, MEDIUMINT, INT and DECIMAL.
+	//CHAR, VARCHAR
+	//DATETIME
 	
-    public static ArrayList<String> DetermineSchema(String line, ArrayList<ArrayList<String>> rows) {
-    	
-    	//System.out.println(line);
-    	
-    	ArrayList<String> cols = new ArrayList<String>();
-    	
-    	//add to arraylist using split operation
-    	if (line != null) {
-    		String[] parseLine = line.split("\\s*,\\s*");
-    		for (int i = 0; i < parseLine.length; i++) {
-    			if (!(parseLine[i] == null) || (parseLine[i].length() == 0)) {
-    				cols.add(parseLine[i].trim());
-    			}
-    		
-    			
-    		}	
-    	
-    	}
-    	
-    	rows.add(cols);
-    	return cols;
-    	
-    }
-    
+	static Pattern INT = Pattern.compile("[\\+\\-]?\\d+");
+	static Pattern DECIMAL = Pattern.compile("[\\+\\-]?\\d+\\.\\d+(?:[eE][\\+\\-]?\\d+)?");
+	static Pattern CHAR = Pattern.compile("[^0-9]");
+	static Pattern DDMMYYYY = Pattern.compile("(0?[1-9]|[12][0-9]|3[01])/(0?[1-9]|1[012])/((19|20)\\d\\d)");
+	static Pattern MMDDYYYY = Pattern.compile("(0?[1-9]|1[012])/(0?[1-9]|[12][0-9]|3[01])/((19|20)\\d\\d)");
+	static Pattern MMDDYY = Pattern.compile("(0?[1-9]|1[012])/(0?[1-9]|[12][0-9]|3[01])/(\\d\\d)");
+	static Pattern HOUR24 = Pattern.compile("([01]?[0-9]|2[0-3]):[0-5][0-9]");
+	static Pattern HOUR12 = Pattern.compile("(1[012]|[1-9]):[0-5][0-9](\\s)?(?i)(am|pm)");
+	static Pattern IPADDRESS = Pattern.compile("([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])");
+	static Pattern EMAIL = Pattern.compile("[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})");
+	
+   public static void getSchema(String filepath)throws IOException{
+
+	   File file = new File(filepath);
+       file.createNewFile();
+       FileReader input = new FileReader(file); 
+       
+       CSVParser parser = new CSVParser(input);
+       Object[] values = parser.getLine(); 
+       while (parser.getLineNumber() <= 30) {
+    	   int numCols = 0;
+    	   for (Object col: values) {
+    		   numCols++; 
+    		   DatabaseGUI.fileContents.append(col + "\t"); 
+    	   }
+    	   if (DatabaseGUI.maxCols < numCols) { 
+    		   DatabaseGUI.maxCols = numCols; 
+    		   for (Object col: values) {
+    			   DatabaseGUI.defaultVals.add(col);
+    		   }
+    	   }
+    	   DatabaseGUI.fileContents.append("\n");
+		   values = parser.getLine();
+       }
+       input.close();
+   }
+  
+   
+   public static void guessTitles(ArrayList<Object> defaultVals) {
+	   for (Object header : defaultVals) {
+		   String test = (String)header;
+		   
+		   if (CHAR.matcher(test).matches()) {
+			   System.out.print(" char ");
+		   }
+		   else if (DECIMAL.matcher(test).matches()) {
+			   System.out.print(" decimal ");
+		   }
+		   else if (INT.matcher(test).matches()) {
+			   System.out.print(" int ");
+		   }
+		   else if (DDMMYYYY.matcher(test).matches()) {
+			   System.out.print(" ddmmyyyy ");
+		   }
+		   else if (MMDDYYYY.matcher(test).matches()) {
+			   System.out.print(" mmddyyyy ");
+		   }
+		   else if (MMDDYY.matcher(test).matches()) {
+			   System.out.print(" mmddyy ");
+		   }
+		   else if (HOUR24.matcher(test).matches()) {
+			   System.out.print(" hour24 ");
+		   }
+		   else if (HOUR12.matcher(test).matches()) {
+			   System.out.print(" hour12 ");
+		   }
+		   else if (IPADDRESS.matcher(test).matches()) {
+			   System.out.print(" ipaddress ");
+		   }
+		   else if (EMAIL.matcher(test).matches()) {
+			   System.out.print(" email ");
+		   }
+	   }
+   }
 }
