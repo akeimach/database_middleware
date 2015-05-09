887125598e5598b71323bcce0e4c4dcb1db6c10d
akeimach <akeimach@umich.edu>
2015-05-09 11:50:16 -0400

    implemented change data type

--- ChangeSchema.java
+++ ChangeSchema.java
@@ -7,25 +7,25 @@ import java.util.Vector;
 
 public class ChangeSchema extends Connect {
 
-	public static Map<Integer, String> renameMap;
-	public static Map<Integer, String> changeType;
+	public static Map<Integer, String> renameColMap;
+	public static Map<Integer, String> changeTypeMap;
 
 	@SuppressWarnings("rawtypes")
 	public static void verifyChanges(Vector titles, Vector types) {
 
-		renameMap = new HashMap<Integer, String>();
-		changeType = new HashMap<Integer, String>();
+		renameColMap = new HashMap<Integer, String>();
+		changeTypeMap = new HashMap<Integer, String>();
 
 		for (int i = 0; i < titles.size(); i++) {
-			if (!titles.elementAt(i).equals(Struct.userFields[i])) { renameMap.put(i, (String)titles.elementAt(i)); }
-			if (!types.elementAt(i).equals(Struct.userTypes[i])) { changeType.put(i, (String)types.elementAt(i)); }
+			if (!titles.elementAt(i).equals(Struct.userFields[i])) { renameColMap.put(i, (String)titles.elementAt(i)); }
+			if (!types.elementAt(i).equals(Struct.userTypes[i])) { changeTypeMap.put(i, (String)types.elementAt(i)); }
 		}
 	}
 
 
 	public static void changeColNames() throws SQLException {
-		if (!renameMap.isEmpty()) {
-			for (Entry<Integer, String> entry : renameMap.entrySet()) {
+		if (!renameColMap.isEmpty()) {
+			for (Entry<Integer, String> entry : renameColMap.entrySet()) {
 				String renameCols = "ALTER TABLE " + Struct.tableName + " CHANGE " + Struct.userFields[entry.getKey()] + " " + entry.getValue() + " " + Struct.userTypes[entry.getKey()];
 				Struct.userFields[entry.getKey()] = entry.getValue(); //change so don't redo same one again  
 				executeUpdate(renameCols); 
@@ -34,20 +34,44 @@ public class ChangeSchema extends Connect {
 	}
 
 	public static void changeTypes() throws SQLException {
+		boolean noComma = true;
+		if (!changeTypeMap.isEmpty()) {
+			String changeType = "ALTER TABLE " + Struct.tableName;
+			for (Entry<Integer, String> entry : changeTypeMap.entrySet()) {
 
+				if (noComma || (changeTypeMap.size() == 1)) {
+					noComma = false;
+					changeType += " MODIFY COLUMN " + Struct.userFields[entry.getKey()] + " " + entry.getValue();
+					Struct.userTypes[entry.getKey()] = entry.getValue();
+				}
+				else { 
+					changeType += ", MODIFY COLUMN " + Struct.userFields[entry.getKey()] + " " + entry.getValue();
+					Struct.userTypes[entry.getKey()] = entry.getValue();
+				}
+
+			}
+			executeUpdate(changeType);
+		}
 	}
+
 
 	@SuppressWarnings("rawtypes")
 	public static void mainSchema(final Vector titles, final Vector types) throws SQLException {
-		Thread schemaThread = new Thread() {
+
+		verifyChanges(titles, types);
+
+		Thread changeColThread = new Thread() {
 			public void run() {
-				verifyChanges(titles, types);
-				try { changeColNames(); } 
+				try { 
+					changeColNames(); 
+					changeTypes(); 
+				} 
 				catch (SQLException e) { e.printStackTrace(); } 
 			}
 		};
-		schemaThread.setName("schemaThread");
-		schemaThread.start();
+		changeColThread.setName("changeColThread");
+		changeColThread.start();
+
 	}
 
 
