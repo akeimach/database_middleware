db0b0165ed255a0ad975c17ab6dd4c9e02c959eb
akeimach <akeimach@umich.edu>
2015-06-01 21:58:59 -0400

    test KS statistic

--- QueryData.java
+++ QueryData.java
@@ -8,65 +8,68 @@ import java.util.HashMap;
 
 public class QueryData extends Connect {
 
-	//static ResultSet queryRS = null;
 
 	static class queryStats {
 
-		public static void randomness(String query) throws SQLException {
+		public static long num_tuples;
+		public static long k_size;
+		public static HashMap<String, ArrayList<Object>> rsNums;
+
+		public static void collectNumerics(String query) throws SQLException {
 
 			//get num tuples, divide by k
-			long num_tuples = Connect.countRows();
-			long k_size = num_tuples / Struct.k_subsets;
-			System.out.println("First k: " + k_size);
-			System.out.println("Second k: " + (2 * k_size));
+			num_tuples = Connect.countRows();
+			k_size = num_tuples / Struct.k_subsets;
 
 			//repeat query on subdivisions for numeric results
 			//TODO: fix from hard coding
 			statsQueryTable(executeQuery(query));
 			//do shit
-			publishQuery(executeQuery(query));
-
-
 
 		}
 
-	}
+		public static void createCDF(HashMap numericMap) {
 
-	
-	public static void statsQueryTable(ResultSet rs) throws SQLException {
-		try {
-			
-			ResultSetMetaData metaData = rs.getMetaData();
-			int numberOfColumns = metaData.getColumnCount(); 
-			HashMap<String, ArrayList<Object>> rsNums = new HashMap<String, ArrayList<Object>>();
-			ArrayList<Object> contents = new ArrayList<Object>();
-			// Get the column names, only show RS which matches userFields
-			for (int col = 1; col <= numberOfColumns; col++) {
-				//ArrayList newEmpty = new ArrayList<Integer>();
-				int type = metaData.getColumnType(col);
-				if ((type == Types.BIGINT) || (type == Types.DECIMAL) || (type == Types.DOUBLE) || (type == Types.FLOAT) || (type == Types.NUMERIC) || (type == Types.INTEGER) || (type == Types.BOOLEAN)) {
-					contents = new ArrayList<Object>();
-					rsNums.put(metaData.getColumnLabel(col), contents); 
-					System.out.print(metaData.getColumnLabel(col) + " ");
-				}
-			}
+		}
 
-			// Get all rows.
-			while (rs.next()) {
-				
-				for (int i = 1; i <= numberOfColumns; i++) { 	
-					if (rsNums.containsKey(metaData.getColumnLabel(i))) {
-						contents = rsNums.get(metaData.getColumnLabel(i));
-						contents.add(rs.getObject(i));
+
+
+		public static void statsQueryTable(ResultSet rs) throws SQLException {
+			try {
+
+				ResultSetMetaData metaData = rs.getMetaData();
+				int numberOfColumns = metaData.getColumnCount(); 
+				rsNums = new HashMap<String, ArrayList<Object>>();
+				ArrayList<Object> contents = new ArrayList<Object>();
+				// Get the column names, only show RS which matches userFields
+				for (int col = 1; col <= numberOfColumns; col++) {
+					//ArrayList newEmpty = new ArrayList<Integer>();
+					int type = metaData.getColumnType(col);
+					if ((type == Types.BIGINT) || (type == Types.DECIMAL) || (type == Types.DOUBLE) || (type == Types.FLOAT) || (type == Types.NUMERIC) || (type == Types.INTEGER) || (type == Types.BOOLEAN)) {
+						contents = new ArrayList<Object>();
+						rsNums.put(metaData.getColumnLabel(col), contents); 
+						System.out.print(metaData.getColumnLabel(col) + " ");
 					}
 				}
+
+				// Get all rows.
+				while (rs.next()) {
+
+					for (int i = 1; i <= numberOfColumns; i++) { 	
+						if (rsNums.containsKey(metaData.getColumnLabel(i))) {
+							contents = rsNums.get(metaData.getColumnLabel(i));
+							contents.add(rs.getObject(i));
+						}
+
+					}
+
+					return;
+				} 
 			}
+			catch (Exception e) { e.printStackTrace(); }
 
-			return;
-		} 
-		catch (Exception e) { e.printStackTrace(); }
+		}
 	}
-
 	public static void publishQuery(ResultSet rs) throws SQLException {
 		GUI.queryOutput.setModel(TableForm.queryTable(rs)); 
 		GUI.queryOutput.setColumnModel(TableForm.colwidth(GUI.queryOutput));
@@ -78,8 +81,10 @@ public class QueryData extends Connect {
 		Thread queryThread = new Thread() {
 			public void run() {
 				try {
-					queryStats.randomness(userQuery.trim());
-					//publishQuery(executeQuery(userQuery.trim()));
+					queryStats.collectNumerics(userQuery.trim());
+					queryStats.createCDF(queryStats.rsNums);
+
+					publishQuery(executeQuery(userQuery.trim()));
 
 				} 
 				catch (SQLException e) { e.printStackTrace(); }	
@@ -90,6 +95,7 @@ public class QueryData extends Connect {
 
 		return;
 	}
+
 
 
 }
