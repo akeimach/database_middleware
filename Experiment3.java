494d4fe40c6b778c19112de8dca5d0067a1dec4e
akeimach <akeimach@umich.edu>
2015-06-25 14:52:41 -0400

    organized file structure

--- Experiment3.java
+++ Experiment3.java
@@ -83,7 +83,18 @@ public class Experiment3 {
 		catch (IOException e) { e.printStackTrace(); }
 	}
 
-	////// MAKE TABLE //////
+	public static void deleteShortFile(String directory) {
+		//delete the last file in directory because likely it isnt the same length of tuples
+		File folder = new File("/Users/alyssakeimach/Eclipse/DBconnector/splits/" + directory + "/");
+		File[] roots = folder.listFiles();
+		Arrays.sort(roots);
+		int last = roots.length - 1;
+		Path shortFile = roots[last].toPath();
+		try { Files.delete(shortFile); } 
+		catch (IOException e) { e.printStackTrace(); }
+	}
+
+	////// LOAD RANDOM FILE //////
 	public static void tableInit(String tableName, String tableStmt) {
 		try {
 			String dropString = "DROP TABLE IF EXISTS " + tableName;
@@ -101,8 +112,7 @@ public class Experiment3 {
 		}
 	}
 
-	////// LOAD RANDOM FILE //////
-	public static File getRandFile(File roots) {
+	public static File getRandom(File roots) {
 		//check if multiple files in directory
 		if (roots.isFile() || roots.list().length == 0) { return roots; }
 		File[] files = roots.listFiles();
@@ -114,7 +124,7 @@ public class Experiment3 {
 		Random rand = new Random();
 		while (!sub_dir.isEmpty()) {
 			File rndSubDir = sub_dir.get(rand.nextInt(sub_dir.size()));
-			File rndSubFile = getRandFile(rndSubDir);
+			File rndSubFile = getRandom(rndSubDir);
 			if (rndSubFile != null) {
 				System.out.println(rndSubFile.getAbsolutePath() + 2);
 				return rndSubFile;
@@ -128,7 +138,7 @@ public class Experiment3 {
 		File folder = new File("/Users/alyssakeimach/Eclipse/DBconnector/splits/" + directory + "/");
 		File[] roots = folder.listFiles();
 		Random rand = new Random();
-		File rndFile = getRandFile(roots[rand.nextInt(roots.length)]);
+		File rndFile = getRandom(roots[rand.nextInt(roots.length)]);
 		String loadFile = "LOAD DATA CONCURRENT LOCAL INFILE '" +  rndFile.getAbsolutePath()  + "' INTO TABLE " + tableName + " " + loadStmt;
 		try { 
 			Connection conn = getConnection();
@@ -139,7 +149,7 @@ public class Experiment3 {
 		return rndFile;
 	}
 
-	public static void removeRandom(File rndFile, String directory) {
+	public static void removeRandom(File rndFile) {
 		try {
 			Path source = (Path) rndFile.toPath();
 			Path target = (Path) Paths.get("/Users/alyssakeimach/Eclipse/DBconnector/splits/replacement/", rndFile.getName());
@@ -148,7 +158,7 @@ public class Experiment3 {
 		catch (IOException e) { e.printStackTrace(); } //could not move file
 	}
 
-	////// QUERY TABLE //////
+	////// BLB //////
 	public static HashMap<String, double[]> getRS(ResultSet rs, int b) {
 		HashMap<String, double[]> bsMap = new HashMap<String, double[]>();
 		try {
@@ -179,7 +189,6 @@ public class Experiment3 {
 		return bsMap;
 	}
 
-	////// MATH //////
 	public static double mean(double[] nums, int dataPoints) {
 		double mean = 0.0;
 		for (int i = 0; i < dataPoints; i++) { mean += nums[i]; }
@@ -204,11 +213,11 @@ public class Experiment3 {
 		if (first) {
 			System.out.println("Standard Deviation and mean for each s");
 			System.out.print("s\t");
-			for (Entry<String, double[]> entry : s_avgs.entrySet()) { System.out.print(entry.getKey() + " standard dev\tMean\t\t\t"); }
+			for (Entry<String, double[]> entry : s_avgs.entrySet()) { System.out.print(entry.getKey() + " standard dev\tMean\t\t\t\t"); }
 			System.out.println();
 			first = false;
 		}
-		
+
 		double var2 = 0.0;
 		System.out.print(s_index+1 + "\t");
 		for (Entry<String, double[]> entry : s_avgs.entrySet()) {  
@@ -221,7 +230,7 @@ public class Experiment3 {
 			}
 			var2 = (var2 / (s - 1)); //unbiased sample variance
 			double std_dev = Math.sqrt(var2);
-			System.out.print(std_dev + "\t" + s_field_mean + "\t\t\t");
+			System.out.print(std_dev + "\t" + s_field_mean + "\t\t\t\t");
 			addValue(s_stdev, entry.getKey(), s, s_index, std_dev);
 		}
 		System.out.println();
@@ -229,58 +238,57 @@ public class Experiment3 {
 	}
 
 	////// MAINS //////
-	public static void mainRestart(String directory, final String fileName, final int b) throws InterruptedException {
+	public static void mainSplit(String directory, final String fileName, final int b) throws InterruptedException {
+		//splitFile, deleteShortFile
 		File folder = new File("/Users/alyssakeimach/Eclipse/DBconnector/splits/" + directory + "/");
 		for(File file: folder.listFiles()) file.delete();
 		Thread.sleep(1000);
 		final File file = new File("/Users/alyssakeimach/" + fileName);
 		splitFile(file, directory, b); 
 		Thread.sleep(3000);
+		deleteShortFile(directory);
+		Thread.sleep(500); //half second
 	}
 
-	
-	public static void mainStats(String directory, String s_tableName, String tableStmt, String loadStmt, int n, int s, int b) throws SQLException, FileNotFoundException {
-		
-		s_avgs = new HashMap<String, double[]>();
-		s_stdev = new HashMap<String, double[]>();
-		s_stderr = new HashMap<String, double[]>();
-
-		for (int s_index = 0; s_index < s; s_index++) {
-			//take random sample size b from dataset
-			tableInit(s_tableName, tableStmt);
-			File rnd = loadRandom(s_tableName, loadStmt, directory);
-			//without replacement
-			removeRandom(rnd, directory);
-
-			//for each s make a bootstrapped sample size n
-			HashMap<String, double[]> rs = new HashMap<String, double[]>();
-			Connection conn = getConnection();
-			rs = getRS(executeQuery(conn, "SELECT * FROM " + s_tableName), b);
-			for (Entry<String, double[]> entry : rs.entrySet()) {  
-
-				double[] n_durations = new double[n];
-				double[] b_durations = entry.getValue();
-
-				//select n random durations and find the average over all n
-				for (int i = 0; i < n; i++) {
-					Random rand = new Random();
-					int randomNum = rand.nextInt(b);
-					n_durations[i] = b_durations[randomNum];
-				}
-
-				double mean = mean(n_durations, n);
-				addValue(s_avgs, entry.getKey(), s, s_index, mean);
-			}	
-			s_stdev = standardDeviation(s_avgs, s, s_index);
-			conn.close();
-		}
+	public static void mainLoad(String directory, String tableName, String tableStmt, String loadStmt) {
+		//take random sample size b from dataset
+		tableInit(tableName, tableStmt);
+		File rnd = loadRandom(tableName, loadStmt, directory);
+		removeRandom(rnd); //without replacement)
 	}
+
+	public static void mainBLB(String tableName, int n, int s, int b, int s_index) {
+		//for each s make a bootstrapped sample size n
+		HashMap<String, double[]> rs = new HashMap<String, double[]>();
+		Connection conn = getConnection();
+		try { rs = getRS(executeQuery(conn, "SELECT * FROM " + tableName), b); } 
+		catch (SQLException e) { e.printStackTrace(); }
+		for (Entry<String, double[]> entry : rs.entrySet()) {  
+
+			double[] n_durations = new double[n];
+			double[] b_durations = entry.getValue();
+
+			//select n random durations and find the average over all n
+			for (int i = 0; i < n; i++) {
+				Random rand = new Random();
+				int randomNum = rand.nextInt(b);
+				n_durations[i] = b_durations[randomNum];
+			}
+
+			double mean = mean(n_durations, n);
+			addValue(s_avgs, entry.getKey(), s, s_index, mean);
+		}	
+		s_stdev = standardDeviation(s_avgs, s, s_index);
+		try { conn.close(); } 
+		catch (SQLException e) { e.printStackTrace(); }
+	}
+
 
 	public static void main(String args[]) throws SQLException, InterruptedException, FileNotFoundException  {
 
 		int n = 21000; //tuples in 15% of original dataset
-		int s = 30; //number of subsamples taken from n
-		int b = 500; //size of s in tuples
+		int s = 10; //number of subsamples taken from n
+		int b = 600; //size of s in tuples
 
 		////// TRIP DATA //////
 		int A = 15; //percentage of original dataset
@@ -300,8 +308,15 @@ public class Experiment3 {
 		final String loadStmt = "FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' (_station_id_, _bikes_available_, _docks_available_, _time_) SET id_0 = NULL";
 		 */
 
-		mainRestart(directory, fileName, b);
-		mainStats(directory, tableName, tableStmt, loadStmt, n, s, b);
+		mainSplit(directory, fileName, b); //deletes old shit
+		s_avgs = new HashMap<String, double[]>();
+		s_stdev = new HashMap<String, double[]>();
+		s_stderr = new HashMap<String, double[]>();
+
+		for (int s_index = 0; s_index < s; s_index++) {
+			mainLoad(directory,tableName, tableStmt, loadStmt);
+			mainBLB(tableName, n, s, b, s_index);
+		}
 	}
 
 }
